8136447c78426945e4373c320650f65b2c0fdb50
MR. YADAV <163430575+yadavanu63@users.noreply.github.com>
2024-04-30 11:43:15 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "24478182"
 api_hash = "a98b5be0127986be1cc2553dbd99765e"
-bot_token = "6527897155:AAGo1PZIFWFAaJf69CkUrEtl0SyHCw88y1E"
+bot_token = "6588487608:AAHq0MxdmBCBYTKRYSXgDKewJu0LAWFersw"
